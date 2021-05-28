b944c5123e2e5d1fbd2f11498e95d48b8fc32f2c
Kyuhee Jo <kyuhee0622@gmail.com>
2021-05-28 17:17:02 +0900

    git_ignore

--- utils.py
+++ utils.py
@@ -31,6 +31,8 @@ import torch
 from torch import nn
 import torch.distributed as dist
 from PIL import ImageFilter, ImageOps
+import argparse
+import warnings 
 
 
 class GaussianBlur(object):
@@ -516,7 +518,7 @@ def _no_grad_trunc_normal_(tensor, mean, std, a, b):
 
 
 def trunc_normal_(tensor, mean=0., std=1., a=-2., b=2.):
-    # type: (Tensor, float, float, float, float) -> Tensor
+    # (Tensor, float, float, float, float) -> Tensor
     return _no_grad_trunc_normal_(tensor, mean, std, a, b)
 
 
