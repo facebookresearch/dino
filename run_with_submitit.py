f5be45e3262b8c1d0f8bece523da263c6a4d01a4
MikeMACintosh <85686842+MikeMACintosh@users.noreply.github.com>
2022-10-11 15:21:04 +0300

    update run_with_submitit.py
    Setting default value to environment variable and getting absolute path instead of relative path to avoid error

--- run_with_submitit.py
+++ run_with_submitit.py
@@ -38,9 +38,10 @@ def parse_args():
 
 
 def get_shared_folder() -> Path:
-    user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    user = os.getenv("USER", default='user')
+    cur_file_path = Path("checkpoint").absolute()
+    if Path("checkpoint/").is_dir():
+        p = Path(f"{cur_file_path}/{user}/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
